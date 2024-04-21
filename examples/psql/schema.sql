d90571424a17da8234fce1dc1dfbb356dacae79e
Alex Sorokoumov <aleksandr.sorokoumov@gmail.com>
2024-04-21 13:39:39 -0700

    Remove enum tables for config values

--- examples/psql/schema.sql
+++ examples/psql/schema.sql
@@ -1,29 +1,9 @@
-CREATE TABLE IF NOT EXISTS benchmarks (
-    name TEXT PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS branches (
-    name TEXT PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS stores (
-    name TEXT PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS instance_types (
-    name TEXT PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS scenarios (
-    name TEXT PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS configs (
     id SERIAL PRIMARY KEY,
-    benchmark TEXT NOT NULL REFERENCES benchmarks(name),
-    scenario TEXT NOT NULL REFERENCES scenarios(name),
-    store TEXT NOT NULL REFERENCES stores(name),
-    instance_type TEXT NOT NULL REFERENCES instance_types(name),
+    benchmark TEXT NOT NULL,
+    scenario TEXT NOT NULL,
+    store TEXT NOT NULL,
+    instance_type TEXT NOT NULL,
     cache BOOLEAN NOT NULL,
     UNIQUE(benchmark,
            scenario,
@@ -35,7 +15,7 @@ CREATE TABLE IF NOT EXISTS configs (
 CREATE TABLE IF NOT EXISTS experiments (
     id TEXT PRIMARY KEY,
     ts TIMESTAMPTZ NOT NULL,
-    branch TEXT NOT NULL REFERENCES branches(name),
+    branch TEXT NOT NULL,
     commit TEXT NOT NULL,
     commit_ts TIMESTAMPTZ NOT NULL,
     username TEXT NOT NULL,
